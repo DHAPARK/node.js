807acba2130f6904e461054242e19384b9b4fe87
DHPARK <37052379+DHAPARK@users.noreply.github.com>
2021-11-02 00:30:14 +0900

    Add files via upload

---
+++ study9/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    server_port: 3000,
+    db_url: 'mongodb://localhost:27017/nodedb',
+    db_schemas: [{file:'./member_schema', collection:'member2', schemaName:'MemberSchema', modelName:'MemberModel'}],
+    route_info: [],
+    facebook: {
+        clientID: '404644524064140',
+        clientSecret: '4bada4bec354776c25d37a4de11491c6Rese',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    }
+}
